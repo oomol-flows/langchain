0e3daff8ae44194281094da77473cf5f25d6f1a8
TaoZeyu <i@taozeyu.com>
2024-12-26 10:17:04 +0800

    feat(blocks): move prompt to the head of chain

--- tasks/prompt/main.py
+++ tasks/prompt/main.py
@@ -2,7 +2,4 @@ from typing import Any
 from langchain_core.prompts import PromptTemplate
 
 def main(params: dict):
-  input_params: dict[str, Any] = params["params"]
-  template = PromptTemplate.from_template(params["template"])
-  prompt = template.invoke(**input_params)
-  return { "prompt": prompt }
+  return { "output": PromptTemplate.from_template(params["template"]) }
